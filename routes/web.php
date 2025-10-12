4129f3854e10a165c3aae63480ec73ebc0396b66
Angga Dhammika Jaya <anggadj94@gmail.com>
2025-10-12 22:34:46 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\WoodController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () { return view('home'); });
-Route::get('/woods', function () { return view('woods'); });
-Route::get('/services', function () { return view('services'); });
+Route::get('/woods', [WoodController::class, 'index']);
+Route::get('/services', [ServiceController::class, 'index']);
 Route::get('/contacts', function () { return view('contacts'); });
 
 
