91d0c64b789e2ae6f479fc974c09fe09d28b7e4f
Angga Dhammika Jaya <anggadj94@gmail.com>
2025-10-12 22:34:31 +0700

    Update 2025_10_11_081425_create_wood_table.php

--- database/migrations/2025_10_11_081425_create_wood_table.php
+++ database/migrations/2025_10_11_081425_create_wood_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('wood', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->text('image')->nullable();
             $table->timestamps();
         });
     }
