1d34861e42200b6a3eb02d5cd6f54236f629958c
Angga Dhammika Jaya <anggadj94@gmail.com>
2025-10-12 22:34:16 +0700

    Update WoodController.php

--- app/Http/Controllers/WoodController.php
+++ app/Http/Controllers/WoodController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Wood;
 
 class WoodController extends Controller
 {
-    //
+    public function index()
+    {
+        $woods = Wood::all();
+        return view('woods', compact('woods'));
+    }
 }
