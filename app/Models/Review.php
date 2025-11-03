b69cfbbd84e41aff392002bd48ae37e47f6037a5
Infinite-cpu-ai <hanzelius01@student.ciputra.ac.id>
2025-11-03 09:23:11 +0700

    pushhh itu anu user

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,4 +9,16 @@ class Review extends Model
 {
     /** @use HasFactory<\Database\Factories\ReviewFactory> */
     use HasFactory;
+    
+    private $name;
+    private $description;
+    private $image;
+    private $price;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+        'price'
+    ];
 }
