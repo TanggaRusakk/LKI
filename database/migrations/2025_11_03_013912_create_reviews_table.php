02a4b665ac9d97d8b0f3863c10553594f2c85e65
Infinite-cpu-ai <hanzelius01@student.ciputra.ac.id>
2025-11-10 01:33:08 +0700

    migrate changes

--- database/migrations/2025_11_03_013912_create_reviews_table.php
+++ database/migrations/2025_11_03_013912_create_reviews_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('service_id')->constrained('services')->onDelete('cascade');
             $table->string('title');
             $table->integer('rating'); // 1-5
             $table->text('comment');
